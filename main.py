cba91fe25be09e5d2e9d02f6ce485b547cc93e3d
SamMugel <sam.mugel@multiversecomputing.com>
2023-05-04 15:53:57 -0400

    added log

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from relay_control import RelayControl
 
 def log(decision_to_water: bool):
     log = "Today's date, {0}\n".format(date.today())
-    log += "decision to water: {0}\n\n".format(decision_to_water)
+    log += "decision to water, {0}\n\n".format(decision_to_water)
     for i in range(2):
         log += "day index, {0}\n".format(i)
         log += "date, {0}\n".format(check_weather.date(i))
@@ -22,5 +22,5 @@ if __name__ == '__main__':
     decision_to_water = should_i_water(check_weather.passed_rain())
     if decision_to_water:
         RelayControl().activate(watering_time)
-        
+
     print(log(decision_to_water))
