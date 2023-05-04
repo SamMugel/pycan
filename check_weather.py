adf89967d63bf439aafe720051bbd97c4aa269c3
SamMugel <sam.mugel@multiversecomputing.com>
2023-05-04 15:18:33 -0400

    added decision to water and type hints

--- check_weather.py
+++ check_weather.py
@@ -21,7 +21,7 @@ class CheckWeather:
         self.owm = pyowm.OWM(self.pyown_api_key)
         self.daily_forecast = self.get_daily_forecast()
 
-    def get_coords(self):
+    def get_coords(self) -> tuple:
         mgr = self.owm.geocoding_manager()
         # geocode London (no country specified) - we'll get many results
         list_of_locations = mgr.geocode(self.location)
@@ -33,17 +33,33 @@ class CheckWeather:
         one_call = manager.one_call(*self.get_coords())
         return one_call.forecast_daily
 
-    def date(self, day_index: int = 0):
+    def date(self, day_index: int = 0) -> str:
         date = self.daily_forecast[day_index].ref_time
         return convert_timestamp(date)
 
-    def daily_precipitation(self, day_index: int = 0):
+    def daily_precipitation(self, day_index: int = 0) -> float:
         precipitation = self.daily_forecast[day_index].rain
         return read_precipitation(precipitation)
 
     def daily_weather(self, day_index: int = 0):
         weather = self.daily_forecast[day_index].status
         return weather
+
+    def passed_rain(self) -> float:
+        return self.daily_precipitation(0)
+
+    def future_rain(self) -> float:
+        return self.daily_precipitation(1)
+
+
+# def should_i_water(passed_rain: float, future_rain: float, rain_threshold: float = None):
+def should_i_water(passed_rain: float) -> bool:
+    if passed_rain == 0:
+        water = True
+    else:
+        water = False
+
+    return water
 
 
 if __name__ == '__main__':
@@ -53,5 +69,7 @@ if __name__ == '__main__':
         print("the date in {0} days is {1}".format(i, check_weather.date(i)))
         print("the precipitation in {0} days is {1}mm".format(i, check_weather.daily_precipitation(i)))
         print("the weather in {0} days is {1}".format(i, check_weather.daily_weather(i)))
+
+    print("decision to water: %s" % should_i_water(check_weather.passed_rain()))
 
 
